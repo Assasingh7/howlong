c2fa4d5c749965635aa23d07f36bb51f5cd7bc14
assasinghriskprofiler <assasingh2003@gmail.com>
2026-07-15 07:08:00 +0530

    Ding ding

--- arrays/longest_subarray_with_k_positive.py
+++ arrays/longest_subarray_with_k_positive.py
@@ -1,1 +1,24 @@
-def main(arr):
+def main(arr, k):
+    n = len(arr)
+    leng = 0
+    for i in range(n):
+        for j in range(i, n):
+            sum = 0
+            for l in range(i, j+1):
+                sum+=arr[l]
+            if sum==k:
+                leng = max(leng, j-i+1)
+    return leng
+
+def mainn(arr, k):
+    n = len(arr)
+    leng = 0
+    for i in range(n):
+        sum = 0
+        for j in range(i, n):
+            sum+=arr[j]
+            if sum==k:
+                leng = max(leng, j-i+1)
+    return leng
+if __name__ == "__main__":
+    print(mainn([10, 5, 2, 7, 1, 9], 15))
